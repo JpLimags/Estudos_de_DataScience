1ab094004225b8cbd81451d6d450cd994837c5b6
JpLimags <joaopedro66.fortal@gmail.com>
2023-02-15 14:30:26 -0300

    Adicionando os materiais dos estudo de listas

---
+++ estrutura_dados_python/lista.py
@@ -0,0 +1,4 @@
+usuario_web = ['Pablo_hernandez', 'pbl.nandez', 'pablo555', 'pablo.hernandez@gmail.com']
+
+print(usuario_web)
+print(type(usuario_web))
---
+++ estrutura_dados_python/lista_ex2.py
@@ -0,0 +1,7 @@
+idade = 20 
+sald_em_conta = 868.85
+usuario_loggedin = True
+
+usuario_web = usuario_web = ['Pablo_hernandez', 'pbl.nandez', 'pablo555', 'pablo.hernandez@gmail.com', sald_em_conta,usuario_loggedin]
+
+print(usuario_web)
